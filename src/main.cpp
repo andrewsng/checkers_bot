769094ad3414d23bc6bb5248ce64ce9f7597db13
Andrew Ng <ngandrew1337@gmail.com>
2022-02-01 04:36:46 -0900

    Set up SFML library

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,47 @@
 #include "checkersgame.hpp"
 #include <iostream>
+#include <cmath>
+#include <SFML/Graphics.hpp>
 
 
 int main(int argc, char *argv[]) {
-    CheckersGame game{};
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Checkers");
+    sf::View view = window.getDefaultView();
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            if (event.type == sf::Event::Resized) {
+                sf::FloatRect visibleArea(0.0f, 0.0f,
+                        static_cast<float>(event.size.width),
+                        static_cast<float>(event.size.height));
+                window.setView(sf::View(visibleArea));
+            }
+        }
+        window.clear(sf::Color::Black);
+
+        auto size = window.getSize();
+        auto boardSize = std::min(size.x, size.y);
+        sf::RectangleShape rectangle(sf::Vector2f(boardSize, boardSize));
+        rectangle.setFillColor(sf::Color(240, 215, 180));
+        window.draw(rectangle);
+        for (int tile = 0; tile < 32; ++tile) {
+            sf::RectangleShape r(sf::Vector2f(boardSize / 8.0f, boardSize / 8.0f));
+            r.setFillColor(sf::Color(140, 70, 45));
+            int x = tile % 4;
+            int y = 7 - (tile / 4);
+            if (y % 2 == 0) {
+                r.move((x * boardSize / 4.0f) + boardSize / 8.0f, y * boardSize / 8.0f);
+            }
+            else {
+                r.move(x * boardSize / 4.0f, y * boardSize / 8.0f);
+            }
+            window.draw(r);
+        }
+        window.display();
+    }
+    /* CheckersGame game{};
     while (true) {
         while (true) {
             game.printBoard();
@@ -23,6 +61,6 @@ int main(int argc, char *argv[]) {
             }
             std::cout << "Illegal Move.\n";
         }
-    }
+    } */
     return 0;
 }
