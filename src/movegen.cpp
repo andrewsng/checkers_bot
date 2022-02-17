60b90ea3210909cde0e9fc68a4fa245e75218532
Andrew Ng <ngandrew1337@gmail.com>
2022-02-17 01:17:36 -0900

    Add multi-jumps to move generation

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -6,11 +6,12 @@ using std::vector;
 
 
 // Forward declarations
-void generateLeftRight(const Board &, int, vector<Move> &, Board::Tile, bool);
+vector<Move> generateLeftRight(const Board &, Board::Tile, int, bool, bool);
 optional<Board::Tile> getLeft(const Board &, Board::Tile, bool);
 optional<Board::Tile> getRight(const Board &, Board::Tile, bool);
-void generateNextMove(const Board &, int, vector<Move> &, Board::Tile, bool,
-                      decltype(getLeft) *);
+vector<Move> generateNextMove(const Board &, Board::Tile, int,
+                              bool, bool, decltype(getLeft) *);
+void appendMoves(vector<Move> &&source, vector<Move> &dest);
 
 
 vector<Move> generateMoves(const Board &board, int player) {
@@ -18,35 +19,70 @@ vector<Move> generateMoves(const Board &board, int player) {
     bool isRed = (player == 0);
     auto positions = isRed ? board.getRedPositions() : board.getBlackPositions();
     for (const auto &tile : positions) {
-        generateLeftRight(board, player, moves, tile, !isRed);
-        /* if (board.isKing(tile)) {
-            generateLeftRight(board, player, moves, tile, isRed);
-        } */
+        appendMoves(generateLeftRight(board, tile, player, !isRed, false), moves);
+        if (board.isKing(tile)) {
+            appendMoves(generateLeftRight(board, tile, player, isRed, false), moves);
+        }
     }
-    /* auto isJump = [](const Move &move) { return move.isAJump(); };
+    auto isJump = [](const Move &move) { return move.isAJump(); };
     if (std::find_if(moves.begin(), moves.end(), isJump) != moves.end()) {
         moves.erase(std::remove_if(moves.begin(), moves.end(), std::not_fn(isJump)), moves.end());
-    } */
+    }
     return moves;
 }
 
-void generateLeftRight(const Board &board, int player, vector<Move> &moves,
-                       Board::Tile tile, bool isUpward) {
-    generateNextMove(board, player, moves, tile, isUpward, getLeft);
-    generateNextMove(board, player, moves, tile, isUpward, getRight);
+vector<Move> generateLeftRight(const Board &board, Board::Tile tile,
+                               int player, bool isUpward, bool mustBeJump) {
+    auto moves = generateNextMove(board, tile, player, isUpward, mustBeJump, getLeft);
+    appendMoves(generateNextMove(board, tile, player, isUpward, mustBeJump, getRight), moves);
+    return moves;
 }
 
-void generateNextMove(const Board &board, int player, vector<Move> &moves,
-                  Board::Tile tile, bool isUpward, decltype(getLeft) *getNext) {
-    if (auto nextTile = getNext(board, tile, isUpward)) {
-        if (!board.isOccupied(*nextTile)) {
-            moves.push_back(Move{tile, *nextTile, player});
-        } /* else if (auto nextJump = getNext(board, *nextTile, isUpward)) {
-            if (board.isOpponent(*nextTile, player) && !board.isOccupied(*nextJump)) {
-                moves.push_back(Move{tile, *nextJump, player});
-            }
-        } */
+void appendMoves(vector<Move> &&source, vector<Move> &dest) {
+    dest.insert(dest.end(), std::make_move_iterator(source.begin()),
+                            std::make_move_iterator(source.end()));
+}
+
+vector<Move> generateNextMove(const Board &board, Board::Tile tile, int player,
+        bool isUpward, bool mustBeJump, decltype(getLeft) *getNextTile) {
+    vector<Move> moves{};
+    auto nextTile = getNextTile(board, tile, isUpward);
+    if (!nextTile) {
+        return moves;
     }
+    if (!board.isOccupied(*nextTile) && !mustBeJump) {
+        moves.push_back(Move{ tile, *nextTile });
+        return moves;
+    }
+    auto nextJump = getNextTile(board, *nextTile, isUpward);
+    if (!nextJump) {
+        return moves;
+    }
+    if (board.isOpponent(*nextTile, player) && !board.isOccupied(*nextJump)) {
+        Move firstJump{ tile, *nextJump };
+        if (board.isPromotion(firstJump, player)) {
+            moves.push_back(firstJump);
+        }
+        else {
+            Board jumpTaken{board};
+            jumpTaken.updateBoard(firstJump, player);
+            auto multiJumps = generateLeftRight(jumpTaken, *nextJump, player, isUpward, true);
+            if (jumpTaken.isKing(*nextJump)) {
+                appendMoves(generateLeftRight(jumpTaken, *nextJump, player, !isUpward, true), multiJumps);
+            }
+            if (multiJumps.empty()) {
+                moves.push_back(firstJump);
+            }
+            for (const auto &restOfJump : multiJumps) {
+                Move fullJump{firstJump};
+                fullJump.tiles.insert(fullJump.tiles.end(),
+                                      std::make_move_iterator(restOfJump.tiles.begin()+1),
+                                      std::make_move_iterator(restOfJump.tiles.end()));
+                moves.push_back(fullJump);
+            }
+        }
+    }
+    return moves;
 }
 
 optional<Board::Tile> getLeft(const Board &board, Board::Tile tile, bool up) {
