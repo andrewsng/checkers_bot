7d78793a1a0eea2c9534e3943eb990409d95b90a
Andrew Ng <ngandrew1337@gmail.com>
2022-03-23 03:12:42 -0800

    Add CheckersGame back to CMake; Update main to use CheckersGame

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "checkersdisplay.hpp"
+#include "checkersgame.hpp"
 #include "movegen.hpp"
 #include "searching.hpp"
 #include <iostream>
@@ -6,55 +6,10 @@
 #include <cmath>
 #include <SFML/Graphics.hpp>
 
-void botMove(Board &game) {
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    auto legalMoves = generateMoves(game, game.getPlayer());
-    if (legalMoves.empty()) {
-        game.changeTurn();
-        return;
-    }
-    std::uniform_int_distribution<std::size_t> dist(0ull, legalMoves.size() - 1);
-    auto randomMove = legalMoves[dist(gen)];
-    if (auto move = game.getMoveIfLegal(randomMove, game.getPlayer())) {
-        game.makeMove(randomMove, game.getPlayer());
-        game.changeTurn();
-    }
-}
-
 int main(int argc, char *argv[]) {
-    CheckersDisplay display(1000, 1000, "Checkers");
-    Board game{};
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dist(0, 1);
-    int minimax = dist(gen);
-    int alphabeta = 1 - minimax;
-    display.drawBoard(game, minimax);
-    if (alphabeta == 0) {
-        if (auto botMove = miniMax(game, game.getPlayer(), 8)) {
-            game.makeMove(*botMove, game.getPlayer());
-        }
-        game.changeTurn();
-    }
-    while (display.isOpen()) {
-        display.handleInputs(game);
-        /* if (auto botMove = alphaBeta(game, game.getPlayer(), 10)) {
-            game.makeMove(*botMove, game.getPlayer());
-        }
-        game.changeTurn();
-        display.drawBoard(game, minimax);
-        if (auto botMove = alphaBeta(game, game.getPlayer(), 10)) {
-            game.makeMove(*botMove, game.getPlayer());
-        }
-        game.changeTurn(); */
-        display.drawBoard(game, minimax);
-    }
-    return 0;
-
-
     CheckersGame game{};
     game.setRedPlayer(PlayerType::Human);
-    game.setBlackPlayer(PlayerType::AlphaBeta);
+    game.setBlackPlayer(PlayerType::Human);
     game.runGame();
+    return 0;
 }
