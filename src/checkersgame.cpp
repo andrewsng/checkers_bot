a33632c125f78fd999b0a341d155d0977eef1e40
Andrew Ng <ngandrew1337@gmail.com>
2022-03-23 02:32:09 -0800

    Write attemptMove; Continue to expand CheckersGame

--- src/checkersgame.cpp
+++ src/checkersgame.cpp
@@ -1,4 +1,5 @@
 #include "checkersgame.hpp"
+#include "move.hpp"
 
 
 void CheckersGame::setRedPlayer(PlayerType type)
@@ -7,11 +8,34 @@ void CheckersGame::setRedPlayer(PlayerType type)
 void CheckersGame::setBlackPlayer(PlayerType type)
 {}
 
-void CheckersGame::runGame()
-{
+void CheckersGame::runGame() {
     displayBoard();
-    while (!isOver) {
-        makeMove();
+    while (!isOver()) {
+        handleInputs();
+        attemptMove();
         displayBoard();
     }
+}
+
+void CheckersGame::attemptMove() {
+    auto currPlayerType = (_currPlayer == 0) ? _redPlayerType
+                                             : _blackPlayerType;
+    Move moveToMake{};
+    switch (currPlayerType) {
+        case PlayerType::Human:
+            moveToMake = getHumanMove();
+            break;
+        case PlayerType::MiniMax:
+            moveToMake = getMiniMaxMove();
+            break;
+        case PlayerType::AlphaBeta:
+            moveToMake = getAlphaBetaMove();
+            break;
+    }
+    _board.makeMove(moveToMake, _currPlayer);
+    changeTurn();
+}
+
+void CheckersGame::changeTurn() {
+    _currPlayer = 1 - _currPlayer;
 }
--- src/checkersgame.hpp
+++ src/checkersgame.hpp
@@ -17,10 +17,16 @@ public:
     void setRedPlayer(PlayerType type);
     void setBlackPlayer(PlayerType type);
     void runGame();
+    void attemptMove();
 
 private:
 
+    void changeTurn();
+
     Board _board{};
+    int _currPlayer;
+    PlayerType _redPlayerType{PlayerType::Human};
+    PlayerType _blackPlayerType{PlayerType::Human};
 
 };
 
